81d4664e338a5ed78d3c11bcf4cc87f8a0cfdf89
srikanthmk <srikanthmk1@gmail.com>
2023-10-03 14:39:23 +0530

    4 files modified

--- src/app/standing-table/standing-table.component.ts
+++ src/app/standing-table/standing-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Standing } from '../interfaces/Standing';
 import { StandingService } from '../standing.service';
 
 @Component({
--- src/app/team-history/team-history.component.ts
+++ src/app/team-history/team-history.component.ts
@@ -1,34 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StandingService } from '../standing.service';
-import { FixtureResponse } from 'src/interfaces/FixtureResponse';
-import { Location } from '@angular/common'; 
+import { FixtureResponse } from '../interfaces/FixtureResponse';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-team-history',
   templateUrl: './team-history.component.html',
-  styleUrls: ['./team-history.component.css']
+  styleUrls: ['./team-history.component.css'],
 })
 export class TeamHistoryComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute, private standingService: StandingService, private location: Location) { }
+  constructor(
+    private route: ActivatedRoute,
+    private standingService: StandingService,
+    private location: Location
+  ) {}
   teamId!: number;
   fixtureResponse!: FixtureResponse;
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       this.teamId = params['id'];
       this.getTeamHistory();
     });
-  
   }
-  getTeamHistory(){
+  getTeamHistory() {
     this.standingService.getTeamsLastPlayed(this.teamId).subscribe((data) => {
       console.log(data);
       this.fixtureResponse = data;
     });
   }
   goBack() {
-    this.location.back(); 
-}
-
+    this.location.back();
+  }
 }
